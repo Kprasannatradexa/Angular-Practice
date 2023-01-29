c49542a8a144c2d58dc6eee9b871d10527d75c70
Kprasannatradexa <prasanna@tradexa.co.in>
2023-01-29 17:33:57 +0530

    HideAfterDirective added to structural component.

--- src/app/directives/hide-after.directive.ts
+++ src/app/directives/hide-after.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appHideAfter]'
 })
-export class HideAfterDirective {
+export class HideAfterDirective implements OnInit {
 
-  constructor() { }
+  @Input('appHideAfter') delay = 0;
+
+  @Input('appHideAfterThen') showSection: TemplateRef<any> | null = null;
+
+
+  constructor(
+    private containerRef: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) { }
+
+  ngOnInit(): void {
+    this.containerRef.createEmbeddedView(this.templateRef);
+
+    setTimeout(() => {
+      this.containerRef.clear();
+      if (this.showSection) {
+        this.containerRef.createEmbeddedView(this.showSection);
+      }
+    }, this.delay)
+
+  }
 
 }
