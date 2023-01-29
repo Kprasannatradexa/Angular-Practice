50442042e38913bf5eb02efa851b8b5c1bcaf744
Kprasannatradexa <prasanna@tradexa.co.in>
2023-01-29 17:16:24 +0530

    HideAfterDirective  directive added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StructuralComponent } from './structural/structural.component';
+import { HideAfterDirective } from './hide-after.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StructuralComponent
+    StructuralComponent,
+    HideAfterDirective
   ],
   imports: [
     BrowserModule,
