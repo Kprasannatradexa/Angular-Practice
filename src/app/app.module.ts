f4fc5705c911a983477f345555644e64ab8a9a40
Kprasannatradexa <prasanna@tradexa.co.in>
2023-01-29 17:17:48 +0530

    HideAfterDirective directive moved to directive folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StructuralComponent } from './structural/structural.component';
-import { HideAfterDirective } from './hide-after.directive';
+import { HideAfterDirective } from './directives/hide-after.directive';
 
 @NgModule({
   declarations: [
