7ec9a3811fcce6d7763cb8d5457bcd226c084aad
Kprasannatradexa <prasanna@tradexa.co.in>
2023-01-30 22:11:04 +0530

    Routing added.

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: WelcomeComponent,
+    children: [
+      { path: 'add-user', component: AddUserComponent },
+      { path: 'add-product', component: AddProductComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
