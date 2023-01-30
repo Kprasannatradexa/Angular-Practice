23b4c66fe61f7964e86c81046174d2c4fe21f2ac
Kprasannatradexa <prasanna@tradexa.co.in>
2023-01-30 21:55:25 +0530

    AddUserComponent created.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AddUserComponent } from './add-user/add-user.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddUserComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
